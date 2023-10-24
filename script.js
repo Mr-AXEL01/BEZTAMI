478e1c5e2ff6efd3b38613536fbfea317661bb38
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2023-10-24 15:42:54 +0100

    display a seccess message without refreshing page when adding valid information in the contact form

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ faqs.forEach(faqItem => {
 const form = document.getElementById('form');
 const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
+const msgInput = document.getElementById('message');
 
 
 function onSubmit() {
@@ -25,13 +26,21 @@ function onSubmit() {
   }
 
 
-  if (!emailRegex.test(emailInput.value)) {
+  else if (!emailRegex.test(emailInput.value)) {
     isValid = false;
     alert('Please enter a valid email address.');
     e.preventDefault();
   }
 
-  if (!isValid) {
+  else if (!isValid) {
     e.preventDefault();
   }
+
+ else{
+    alert("sent successfully")
+    nameInput.value = ""
+    emailInput.value = ""
+    msgInput.value = ""
+
+ }
 }
